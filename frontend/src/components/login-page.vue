1131c5e1b2206f305fdf767a14b713aa1a3c60c4
RoudroMehedi <roudro.m@gmail.com>
2023-04-09 00:13:24 +0200

    Redesigned login page

--- frontend/src/components/login-page.vue
+++ frontend/src/components/login-page.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="login-container">
-    <v-card class="login-card">
-      <v-card-title>Login</v-card-title>
-      <v-card-text>
-        <v-form @submit.prevent="login">
-          <v-text-field
-            v-model="userName"
-            label="User Name"
-            type="text"
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="password"
-            label="Password"
-            type="password"
-            required
-          ></v-text-field>
-          <v-btn type="submit" color="primary">Login</v-btn>
-        </v-form>
-      </v-card-text>
+    <v-card class="login-card py-0 px-0 elevation-12">
+      <v-row>
+        <v-col sm="7" class="white"
+          ><div class="px-5 py-5">
+            <v-card-title>Login</v-card-title>
+            <v-form @submit.prevent="login">
+              <v-text-field
+                v-model="userName"
+                label="User Name"
+                type="text"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+              <v-btn rounded outlined dark color="teal accent-4" type="submit"
+                >Login</v-btn
+              >
+            </v-form>
+          </div>
+        </v-col>
+        <v-col sm="5" class="teal accent-4 d-flex">
+          <div class="mt-auto mb-auto">
+            <div class="px-0 py-5">
+              <h2 class="white--text text-center">
+                Don't have a account?<br />Register Now!
+              </h2>
+            </div>
+
+            <div class="text-center">
+              <v-btn rounded outlined dark @click="toRegister">SIGN UP</v-btn>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
     </v-card>
   </div>
 </template>
+
 <script>
 import axios from "axios";
 
@@ -33,6 +53,9 @@ export default {
     };
   },
   methods: {
+    toRegister() {
+      this.$router.push("/");
+    },
     async login() {
       try {
         const response = await axios.post("http://localhost:8000/user/login", {
