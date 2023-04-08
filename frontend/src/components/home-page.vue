969ba7d6b7370e9f2c57ca3cbb3bc686b2a5d0b7
RoudroMehedi <roudro.m@gmail.com>
2023-04-08 23:49:36 +0200

    Updated registration form design

--- frontend/src/components/home-page.vue
+++ frontend/src/components/home-page.vue
@@ -1,80 +1,195 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-card>
-          <v-card-title>
-            <h2 class="text-center">User Registration Portal MONGO</h2>
-          </v-card-title>
-          <v-card-text>
-            <v-form @submit.prevent="register">
-              <v-text-field
-                v-model="user.userName"
-                label="User Name"
-                :rules="[(v) => !!v || 'Name is required']"
-                required
-              ></v-text-field>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-3"
+                        >
+                          Sign in to Expense Tracker
+                        </h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
 
-              <v-text-field
-                v-model="user.password"
-                label="Password"
-                :rules="[(v) => !!v || 'Password is required']"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="user.firstName"
-                label="First Name"
-                :rules="[(v) => !!v || 'First name is required']"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="user.lastName"
-                label="Last Name"
-                :rules="[(v) => !!v || 'Last name is required']"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="user.age"
-                label="Age"
-                :rules="[(v) => !!v || 'Age is required']"
-                required
-              ></v-text-field>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">
+                          Ensure your email for registration
+                        </h4>
+                        <v-form>
+                          <v-text-field
+                            v-model="loginData.userName"
+                            label="User Name"
+                            type="text"
+                            required
+                            prepend-icon="email"
+                            color="teal accent-4"
+                          />
 
-              <v-btn type="submit" color="primary">Register</v-btn>
-            </v-form>
-            <v-btn type="submit" color="primary" @click="toLogin"
-              >Login Now</v-btn
-            >
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">Name</th>
-                <th class="text-left">Password</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(user, index) in result" :key="index">
-                <td>{{ user.userName }}</td>
-                <td>{{ user.password }}</td>
-                <td class="text-right">
-                  <v-btn color="warning" class="mr-2" @click="edit(user)">
-                    Edit
-                  </v-btn>
-                  <v-btn color="error" @click="remove(user)">Delete</v-btn>
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-col>
-    </v-row>
-  </v-container>
+                          <v-text-field
+                            v-model="loginData.password"
+                            label="Password"
+                            type="password"
+                            required
+                            prepend-icon="lock"
+                            color="teal accent-4"
+                          />
+                          <h3 class="text-center mt-4">
+                            Forgot your password ?
+                          </h3>
+                          <v-btn
+                            type="submit"
+                            color="primary"
+                            @click.stop="login"
+                            >Login</v-btn
+                          >
+                        </v-form>
+                      </v-card-text>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-4">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hello, Friend!</h1>
+                        <h5 class="text-center">
+                          Enter your personal details and start journay with us
+                        </h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn
+                          rounded
+                          outlined
+                          dark
+                          type="submit"
+                          @click="step++"
+                          >SIGN UP</v-btn
+                        >
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-4">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back!</h1>
+                        <h5 class="text-center">
+                          To Keep connected with us please login with your
+                          personnel info
+                        </h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="toLogin"
+                          >Sign in</v-btn
+                        >
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-4"
+                        >
+                          Create Account
+                        </h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>mdi mdi-google</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>mdi mdi-facebook</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>mdi mdi-linkedin</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">
+                          Ensure your email for registration
+                        </h4>
+                        <v-form>
+                          <v-text-field
+                            v-model="user.userName"
+                            label="User Name"
+                            prepend-icon="mdi mdi-account-circle"
+                            type="text"
+                            color="teal accent-4"
+                            :rules="[(v) => !!v || 'Name is required']"
+                            required
+                          />
+                          <v-text-field
+                            label="Password"
+                            prepend-icon="mdi mdi-form-textbox-password"
+                            color="teal accent-4"
+                            v-model="user.password"
+                            :rules="[(v) => !!v || 'Password is required']"
+                            required
+                          />
+
+                          <v-text-field
+                            label="First Name"
+                            prepend-icon="mdi mdi-account-box-multiple"
+                            color="teal accent-4"
+                            v-model="user.firstName"
+                            :rules="[(v) => !!v || 'First name is required']"
+                            required
+                          />
+                          <v-text-field
+                            id="password"
+                            label="Last Name"
+                            prepend-icon="mdi mdi-account-box-multiple"
+                            color="teal accent-4"
+                            v-model="user.lastName"
+                            :rules="[(v) => !!v || 'Last name is required']"
+                            required
+                          />
+                          <v-text-field
+                            label="Age"
+                            prepend-icon="mdi mdi-stairs"
+                            name="password"
+                            color="teal accent-4"
+                            v-model="user.age"
+                            :rules="[(v) => !!v || 'Age is required']"
+                            required
+                          />
+                          <div class="ml-auto d-flex justify-content-center">
+                            <v-btn
+                              class="ml-auto mr-auto"
+                              text-center
+                              mt-n5
+                              rounded
+                              color="teal accent-4"
+                              dark
+                              type="submit"
+                              @click="register"
+                            >
+                              Register
+                            </v-btn>
+                          </div>
+                        </v-form>
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -87,6 +202,12 @@ export default {
   name: "UserCrud",
   data() {
     return {
+      loginData: {
+        userName: "",
+        password: "",
+      },
+
+      step: 2,
       result: {},
       user: {
         _id: "",
@@ -97,6 +218,9 @@ export default {
         age: "",
       },
     };
+  },
+  props: {
+    source: String,
   },
   created() {
     this.UserLoad();
@@ -157,6 +281,28 @@ export default {
     toLogin() {
       this.$router.push("/login");
     },
+  },
+  async login() {
+    try {
+      const response = await axios.post("http://localhost:8000/user/login", {
+        userName: this.loginData.userName,
+        password: this.loginData.password,
+      });
+      // Handle successful login response
+      console.log("Logged in user:", response.data);
+      // Redirect to the dashboard page
+      this.$router.push({
+        path: "/dashboard",
+        query: {
+          loggedInUser: response.data.userName,
+          loggedInUserId: response.data.userId,
+          loggedInUserExpenses: response.data.expenses,
+        },
+      });
+    } catch (error) {
+      // Handle login error
+      console.error(error);
+    }
   },
 };
 </script>
