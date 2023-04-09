a6d931cbedc5975d9c3544a64bdcb4bbc19b02f1
RoudroMehedi <roudro.m@gmail.com>
2023-04-09 15:04:13 +0200

    login menu updated

--- frontend/src/components/login-page.vue
+++ frontend/src/components/login-page.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login-container">
-    <v-card class="login-card py-0 px-0 elevation-12">
+    <v-card class="py-0 px-0 ml-5 mr-5 elevation-12">
       <v-row>
-        <v-col sm="8" md="8" class="white"
+        <v-col cols="12" sm="4" md="8" class="white"
           ><div class="px-5 py-5">
             <v-card-title>Login</v-card-title>
             <v-form @submit.prevent="login">
@@ -35,8 +35,8 @@
             </v-form>
           </div>
         </v-col>
-        <v-col sm="5" class="teal accent-4 d-flex">
-          <div class="mt-auto mb-auto">
+        <v-col sm="5" md="" class="teal accent-4 d-flex">
+          <div class="mt-auto mb-auto ml-auto mr-auto">
             <div class="px-0 py-5">
               <h2 class="white--text text-center">
                 Don't have a account?<br />Register Now!
@@ -103,11 +103,5 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
-}
-
-.login-card {
-  max-width: 900px;
-  min-width: 600px;
-  max-height: 700px;
 }
 </style>
